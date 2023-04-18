88aed10dd8c4ec036e936e418612e5a7ece779cc
Raul Millicay <70416614+rmillicay@users.noreply.github.com>
2023-04-17 23:12:03 -0300

    Actualizacion de la instalacion
    Se ha actualizado la instalacion de docker desde: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04

--- install-docker-ce.sh
+++ install-docker-ce.sh
@@ -1,10 +1,8 @@
-apt -y update
-apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt -y update
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
 apt-cache policy docker-ce
-apt  -y install docker-ce
-
-
-
+sudo apt install docker-ce
+sudo systemctl status docker
